00f4df9e88c862de4285484599fa24090019f7dd
Raphael Kalinowski <r.kalinowski@handelsblattgroup.com>
2025-09-28 21:18:44 +0200

    Initial Commit

---
+++ looped/loopedApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopedApp.swift
+//  looped
+//
+//  Created by Raphael Kalinowsi on 28.09.25.
+//
+
+import SwiftUI
+
+@main
+struct loopedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
